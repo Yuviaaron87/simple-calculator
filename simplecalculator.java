cd84e507704ebdda21fd0207e18c50d3cc0de7cb
YUVARAJ M <yuvarajaaron212@gmail.com>
2025-09-18 13:04:22 +0530

    Add files via upload

---
+++ simplecalculator.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.Scanner;
+
+public static void main(String args[]){
+
+    Scanner scan = new Scanner(System.in);
+
+    double num1;
+    double num2;
+    char operator;
+    double result = 0;
+
+    System.out.print("Enter the First Number: ");
+    num1 = scan.nextDouble();
+
+    System.out.print("Enter the Operator( + , -, * , / , ^): ");
+    operator = scan.next().charAt(0);
+
+    System.out.print("Enter the Second Number: ");
+    num2 = scan.nextDouble();
+
+    switch(operator){
+
+    case '+' -> result = num1 + num2;
+    case '-' -> result = num1 - num2;
+    case '*' -> result = num1 * num2;
+    case '/' -> { 
+        if(num2==0){
+            System.out.println("Cannot divide by Zero!");
+        }
+        else{
+            result = num1 / num2;
+
+        }
+    }
+    case '^' -> result = Math.pow(num1,num2);
+    default  -> {
+        System.out.println("Invalid operator");
+    }
+}
+
+    System.out.println(result);
+
+   
+}
